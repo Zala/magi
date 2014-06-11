c402f6191ace32f816f0cec0053ad4511dc0db0d
Zala Herga <zala.herga@gmail.com>
2014-06-11 17:31:40 +0200

    multiple graphs

--- EURdata.r
+++ EURdata.r
@@ -1,66 +1,58 @@
 data = read.table("EURdata_noDates.txt", sep = "\t", header = TRUE)
 sample <- data[1:5,1:4]
 n <- length(data[,4])
-dates <- data[1]
+
+
+# EURAB6E1Y #################################################################################################################
+
 data$EURAB6E1Y.ICAP <- as.POSIXct(data$EURAB6E1Y.ICAP, format="%Y %m %d")
 data$X.2 <- as.numeric(levels(data$X.2))[data$X.2]
-#rownames(data) <- dates
-
-
-
 
 EURAB6E1Y_ALL <- data[1:4]
-class(EURAB6E1Y_ALL[,1])
+#class(EURAB6E1Y_ALL[,1])
+#class(EURAB6E1Y_ALL[,2])
+#class(EURAB6E1Y_ALL)
+#typeof(EURAB6E1Y_ALL)
 
-EURAB6E1Y_TABLE <- EURAB6E1Y_ALL[2:4]
-EURAB6E1Y_MTS <- ts(EURAB6E1Y_TABLE, start = c(2008,2,11), frequency=360, names = c("ask", "bid", "mid"))
-#EURAB6E1Y_MTS <- ts(EURAB6E1Y_ALL, names = c("date","ask", "bid", "mid"))
-plot(EURAB6E1Y_MTS)
+# BID, ASK, MID in separate graphs (not automated dating)
+#EURAB6E1Y_TABLE <- EURAB6E1Y_ALL[2:4]
+#EURAB6E1Y_MTS <- ts(EURAB6E1Y_TABLE, start = c(2006,90), frequency=280, names = c("ask", "bid", "mid"))
+#plot(EURAB6E1Y_MTS)
 
-#EURAB6E1Y_MID <- EURAB6E1Y_ALL[4]
-EURAB6E1Y_MID <- EURAB6E1Y_ALL[1:2]
-#EURAB6E1Y_XTS <- as.xts(ts(EURAB6E1Y_MID, start= c(2008, 40), frequency=360))
-EURAB6E1Y_XTS <- xts(EURAB6E1Y_MID))
-#EURAB6E1Yts <- ts(EURAB6E1Yvec, frequency=360, start = c(2008,40))
-#print(EURAB6E1Yts)
-plot(as.zoo(EURAB6E1Y_XTS), main="EURAB6E1Y MID")
+EURAB6E1Y_MID <- EURAB6E1Y_ALL[4]
+EURAB6E1Y_MID_XTS <- na.omit(xts(EURAB6E1Y_MID, order.by=EURAB6E1Y_ALL$EURAB6E1Y.ICAP))
+plot(EURAB6E1Y_MID_XTS)
 
-#df2 <- xts(EURAB6E1Y_ALL[2:4], order.by=EURAB6E1Y_ALL$EURAB6E1Y.ICAP)
-
-
-EURAB6E1Yts[20]
-ts(1:10, frequency = 4, start = c(1959, 2))
-plot(EURAB6E1Y[2:1491, "X.2"], ylim = c(0,10), xlim = c(0,1500), pch=".", col="blue")
+EURAB6E1Y_XTS <- na.omit(xts(EURAB6E1Y_ALL[2:4], order.by=EURAB6E1Y_ALL$EURAB6E1Y.ICAP))
+plot(EURAB6E1Y_XTS)
+tsRainbow <- rainbow(3)
+plot.zoo(EURAB6E1Y_XTS, main="EURAB6E1Y ASK, BID, MID RATES", plot.type="single", col=tsRainbow)
 
 
-EURAB6E3Y <- data[,21:24]
-# first observation EURAB6E3Y[3379,]
-EURAB6E3Yts_table <- ts(EURAB6E3Y[,2:4], start = c(1998, 330), frequency=360, names = c("ask", "bid", "mid"))
-plot(EURAB6E3Yts_table)
+# EURAB6E3Y #################################################################################################################
+
+data$EURAB6E3Y.ICAP <- as.POSIXct(data$EURAB6E3Y.ICAP, format="%Y %m %d")
+data$X.17 <- as.numeric(levels(data$X.17))[data$X.17]
+
+EURAB6E3Y_ALL <- data[21:24]
+EURAB6E3Y_MID <- EURAB6E3Y_ALL[4]
+EURAB6E3Y_MID_XTS <- na.omit(xts(EURAB6E3Y_MID, order.by=EURAB6E3Y_ALL$EURAB6E3Y.ICAP))
+plot(EURAB6E3Y_MID_XTS)
+
+EURAB6E3Y_XTS <- na.omit(xts(EURAB6E3Y_ALL[2:4], order.by=EURAB6E3Y_ALL$EURAB6E3Y.ICAP))
+plot(EURAB6E3Y_XTS)
+tsRainbow <- rainbow(3)
+plot.zoo(EURAB6E3Y_XTS, main="EURAB6E3Y ASK, BID, MID RATES", plot.type="single", col=tsRainbow)
+abline(h = -1:5, v = 2000-01-01, col = "lightgray", lty = 3)
+
+
+# MULTIPLE MID RATES #########################################################################################################
+
+MULTIPLE_MID_XTS <- na.omit(xts(cbind(EURAB6E1Y_MID, EURAB6E3Y_MID), order.by=data$EURAB6E1Y.ICAP))
+plot.zoo(MULTIPLE_MID_XTS, main="EURAB6E1Y_MID and EURAB6E3Y_MID", plot.type="single", col=tsRainbow)
+legend(x = "topleft", legend = c("EURAB6E1Y", "EURAB6E3Y"), lty = 1, col = tsRainbow)
+abline(h = -1:5, v = 2000-01-01, col = "lightgray", lty = 3)
 
 
 
 
-
-sp500 <- na.omit( 
-  getSymbols(
-    "SP500",
-    src = "FRED",
-    from = "1949-12-31",
-    auto.assign = FALSE
-  )
-)
-sp500.monthly <- sp500[endpoints(sp500, on ="months")]
-plot(sp500)
-
-timeSeries::plot(
-  timeSeries(sp500.monthly),
-  main = "S&P 500 (timeseries::plot)"
-)
-
-m2 <- cbind(1, 1:4)
-colnames(m2, do.NULL = FALSE)
-colnames(m2) <- c("x","Y")
-rownames(m2) <- c("a", "b", "c", "d")
-rownames(m2) <- rownames(m2, do.NULL = FALSE, prefix = "Obs.")
-m2
